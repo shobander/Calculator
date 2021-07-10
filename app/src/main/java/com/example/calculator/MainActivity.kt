54616796e0486f38b82427d79ecccc7873ea79ed
shobander <personal65-one@yahoo.com>
2021-07-10 10:26:07 +0200

    Implementing Equals operator
    onEquals operator implementation

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,6 +37,42 @@ class MainActivity : AppCompatActivity() {
             tvInput.append(".")
             lastNumeric = false
             lastDot = true
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                    // -215
+                }
+            }
+
+            try {
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0] //99
+                    var two = splitValue[1] //1
+                    // 99 -1
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+
+
+                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
         }
     }
 
